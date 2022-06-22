dc16d5165705f335d75e4c999803d483186146d5
Tartaglia Riccardo <tartaglia.riccardo@gmail.com>
2022-06-22 10:54:05 +0200

    init core

--- core/plugins/posts/index.js
+++ core/plugins/posts/index.js
@@ -5,25 +5,25 @@ const fp = require('fastify-plugin')
  * This plugins adds post functionality to the dictation
  */
 module.exports = fp(async function (dictation) {
+  const postsColl = dictation.mongo.db.collection('posts')
+
   dictation.hooks.addFilter('post', 'dictation', async (params) => {
     const {id = null, post = {}} = await params
     const findCondition = {$or: [{id: id}, {slug: id}]}
-    const postsColl = dictation.mongo.db.collection('posts')
     if (!id) {
       throw new Error(`id not sent`)
     }
-    const postFound = await postsColl.findOne(findCondition)
-    if (postFound) {
-      return {id: postFound.id, post: postFound}
+    const postRes = await postsColl.findOne(findCondition)
+    if (postRes) {
+      return {id: postRes.id, post: postRes}
     }
     throw new Error(`Post ${id} not found`)
   }, 1)
 
   dictation.hooks.addFilter('posts', 'dictation', async (params) => {
     const {posts = [], filters = {}} = await params
-    const postsColl = dictation.mongo.db.collection('posts')
-    const postsQuery = await postsColl.find(filters).toArray()
-    return {posts: postsQuery, filters}
+    const postRes = await postsColl.find(filters).toArray()
+    return {posts: postRes, filters}
   }, 1)
 
 }, {dependencies: ['dictation-hooks']})
