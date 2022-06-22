629edd45cd0f9cdc6a4becbb86d16fa98093fe69
Tartaglia Riccardo <tartaglia.riccardo@gmail.com>
2022-06-22 17:47:43 +0200

    init core

--- core/app.js
+++ core/app.js
@@ -20,11 +20,4 @@ module.exports = async function (fastify, opts) {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
 }
--- core/routes/plugin/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/available', async function (request, reply) {
-    return fastify.hooks.applyFilters('registered-plugin', [])
-  })
-}
