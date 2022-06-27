d4284b4948ac2abaa8814b0526cdab24ae528724
Tartaglia Riccardo <tartaglia.riccardo@gmail.com>
2022-06-27 13:04:33 +0200

    auth ehn

--- plugins/auth/routes.js
+++ plugins/auth/routes.js
@@ -3,22 +3,22 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (dictation) {
-    dictation.post('/signin', {
-        schema: {
-          tags: ['auth'],
-        }
-      }, async function (request, reply) {
-        const {token} = await dictation.hooks.applyFilters('sign_in', request.body)
-        reply.send({token})
-      })
+  dictation.post('/signin', {
+    schema: {
+      tags: ['auth'],
+    }
+  }, async function (request, reply) {
+    const {token} = await dictation.hooks.applyFilters('sign_in', request.body)
+    reply.send({token})
+  })
 
-      dictation.post('/create-user', {
-        onRequest: [dictation.isAdmin],
-        schema: {
-          tags: ['auth'],
-        }
-      }, async function (request, reply) {
-        await dictation.hooks.applyFilters('create_user', request.body)
-        reply.send({message: 'User created'})
-      })
+  dictation.post('/create-user', {
+    onRequest: [dictation.isAdmin],
+    schema: {
+      tags: ['auth'],
+    }
+  }, async function (request, reply) {
+    await dictation.hooks.applyFilters('create_user', request.body)
+    reply.send({message: 'User created'})
+  })
 })
